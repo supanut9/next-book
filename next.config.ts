9ea2a9d244a6b3e037c0e9c56f99bec992b0e068
supanut9 <supanut.tanso@gmail.com>
2025-05-29 21:33:25 +0700

    feat(deps): upgrade next package to be canary ( for ppr experimental )

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [{ protocol: 'https', hostname: '*' }],
+  },
+  experimental: {
+    ppr: 'incremental',
   },
 };
 
