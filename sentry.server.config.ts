e297a24f1e7d2850e2b4e7fbb9ba157d822435c9
supanut9 <supanut.tanso@gmail.com>
2025-06-02 14:17:39 +0700

    feat(sentry): integrate with sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://aa5d740a3f452f2e9caf2c795a863936@o4509427361775616.ingest.us.sentry.io/4509427695484928",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
